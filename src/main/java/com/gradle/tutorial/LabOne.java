5e7b2b30910ab049cdbf95682b0d53b2af54714a
EbonyAtropus <tproskow1@my.bcit.ca>
2022-09-06 11:40:41 -0700

    Lab01 test and tutorials

---
+++ src/main/java/com/gradle/tutorial/LabOne.java
@@ -0,0 +1,22 @@
+package com.gradle.tutorial;
+
+public class LabOne {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(convert(i));
+        }
+    }
+
+    public static String convert(int labOne) {
+        if (labOne % 15 == 0) {
+            return "labOne";
+        }
+        if (labOne % 3 == 0) {
+            return "LAB";
+        }
+        if (labOne % 5 == 0) {
+            return "ONE";
+        }
+        return String.valueOf(labOne);
+    }
+}
---
+++ src/test/java/com/gradle/tutorial/LabOneTest.java
@@ -0,0 +1,37 @@
+package com.gradle.tutorial;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LabOneTest {
+    @Test
+    public void LabOneNormalNumbers() {
+
+        LabOne fb = new LabOne();
+        Assertions.assertEquals("1", fb.convert(1));
+        Assertions.assertEquals("2", fb.convert(2));
+    }
+
+    @Test
+    public void LabOneThreeNumbers() {
+
+        LabOne fb = new LabOne();
+        Assertions.assertEquals("LAB", fb.convert(3));
+    }
+
+    @Test
+    public void LabOneFiveNumbers() {
+
+        LabOne fb = new LabOne();
+        Assertions.assertEquals("ONE", fb.convert(5));
+    }
+
+    @Test
+    public void LabOneThreeAndFiveNumbers() {
+
+        LabOne fb = new LabOne();
+        Assertions.assertEquals("ONE", fb.convert(5));
+    }
+}
